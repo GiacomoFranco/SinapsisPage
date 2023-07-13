4a45aad5cef93ff2ea845995afbd09ce6997ad56
joseacordobac <joseacordobac@gmail.com>
2023-07-13 15:45:09 -0500

    navbar

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  logo = '../../../assets/img/logo.png'
+  flagEng = '../../../assets/img/english.png'
+}
