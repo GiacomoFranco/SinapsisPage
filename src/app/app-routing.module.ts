982eed7778a4b16d909cc34930af4b5421c50ded
mateo.castano <mateo.castano@sinapsis.global>
2023-07-18 16:10:22 -0500

    feat: Implement buttonComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/page/home.component';
 import { ServiciosComponent } from './modules/servicios/page/servicios.component';
+import { NosotrosComponent } from './modules/nosotros/page/nosotros.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'servicios',
     component: ServiciosComponent
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent
   },
   {
     path: '**',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { ButtonSinapsisComponent } from './button-sinapsis/button-sinapsis.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    ButtonSinapsisComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    ButtonSinapsisComponent
   ]
 })
 export class SharedModule { }
