6b8fbfa520b05ad17c41d9f3b671c98f321ef07f
mateo.castano <mateo.castano@sinapsis.global>
2023-07-19 17:14:50 -0500

    feat: Implements Sliders in ServicesView

---
+++ src/app/core/slider.mock.ts
@@ -0,0 +1,49 @@
+import { Slider } from '../models/slide.model';
+const sliderData: Slider[] = [
+    {
+        img: '../../../../assets/img/item.svg',
+        title: 'Desarrollo de Software a la medida'
+    },
+    {
+        img: '../../../../assets/img/item1.svg',
+        title: 'Posicionamiento SEO'
+    },
+    {
+        img: '../../../../assets/img/item2.svg',
+        title: 'Desarrollo de Aplicaciones móviles'
+    },
+    {
+        img: '../../../../assets/img/item3.svg',
+        title: 'Diseño y desarrollo web'
+    },
+    {
+        img: '../../../../assets/img/item4.svg',
+        title: 'Gestión de Redes sociales'
+    },
+    {
+        img: '../../../../assets/img/item.svg',
+        title: 'Desarrollo de Software a la medida'
+    },
+    {
+        img: '../../../../assets/img/item1.svg',
+        title: 'Posicionamiento SEO'
+    },
+    {
+        img: '../../../../assets/img/item2.svg',
+        title: 'Desarrollo de Aplicaciones móviles'
+    },
+    {
+        img: '../../../../assets/img/item.svg',
+        title: 'Desarrollo de Software a la medida'
+    },
+    {
+        img: '../../../../assets/img/item1.svg',
+        title: 'Posicionamiento SEO'
+    },
+    {
+        img: '../../../../assets/img/item2.svg',
+        title: 'Desarrollo de Aplicaciones móviles'
+    },
+]
+
+export default sliderData
---
+++ src/app/models/slide.model.ts
@@ -0,0 +1,4 @@
+export interface Slider{
+    img: string,
+    title: string
+}
