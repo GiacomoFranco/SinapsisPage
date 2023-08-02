ab46fed8f600e8f3bc87bb1e46400b611cdbdd78
Santy <santiago.gil@sinapsis.global>
2023-08-02 11:40:23 -0500

    Adding the testimonios section in home view

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './page/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, SharedModule,CarouselModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeModule { }
+export class HomeModule {}
