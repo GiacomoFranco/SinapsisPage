6c69f9fbd3449e9d5357cdeb53b5758113c85ee3
Santy <santiago.gil@sinapsis.global>
2023-08-07 22:26:01 -0500

    composing trabaja con nosotros and working in its responsive

---
+++ src/app/modules/work-with-us/work-with-us.module.ts
@@ -0,0 +1,15 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkWithUsComponent } from './page/work-with-us.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CaracteristicaComponent } from './caracteristica/caracteristica.component';
+import { CaracteristicasComponent } from './caracteristicas/caracteristicas.component';
+import { PerksComponent } from './perks/perks.component';
+import { PerkComponent } from './perk/perk.component';
+
+@NgModule({
+  declarations: [WorkWithUsComponent, CaracteristicaComponent, CaracteristicasComponent, PerksComponent, PerkComponent],
+  imports: [CommonModule, SharedModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class WorkWithUsModule {}
