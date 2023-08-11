c340b23feb0dc828ebcc2a0ed61a9f322186cd10
mateo.castano <mateo.castano@sinapsis.global>
2023-08-11 10:24:31 -0500

    feat: Implements seo service

---
+++ src/app/services/seo.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Meta } from "@angular/platform-browser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeoService {
+
+  constructor(private meta: Meta) { }
+
+  generateFlags(config: any): any {
+    config = {
+      title: '',
+      description: '',
+      site_name: '',
+      image: '',
+      ...config
+    }
+
+    this.meta.updateTag({ property: 'og:type', content: 'article' });
+    this.meta.updateTag({ property: 'og:site_name', content: 'Sitio web Sinapsis' });
+    this.meta.updateTag({ property: 'og:title', content: config.title });
+    this.meta.updateTag({ property: 'og:description', content: config.description });
+    this.meta.updateTag({ property: 'og:image', content: config.image });
+    this.meta.updateTag({ property: 'og:url', content: `https://.../${config.slug}` });
+  }
+}
