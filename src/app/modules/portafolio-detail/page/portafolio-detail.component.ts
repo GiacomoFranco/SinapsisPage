8c4ca5ae9e0a7e950a46dc1480f71cc685a80d4b
mateo.castano <mateo.castano@sinapsis.global>
2023-08-16 16:57:13 -0500

    feat: Change animations

--- src/app/modules/portafolio-detail/page/portafolio-detail.component.ts
+++ src/app/modules/portafolio-detail/page/portafolio-detail.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PortafolioDetail } from '@app/models/portafolioDetail.model';
 import { PortafolioService } from '@app/services/portafolio.service';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-portafolio-detail',
   templateUrl: './portafolio-detail.component.html',
   styleUrls: ['./portafolio-detail.component.scss']
 })
-export class PortafolioDetailComponent implements OnInit {
+export class PortafolioDetailComponent implements OnInit, OnDestroy {
   slug = ''
 
   dataPage: PortafolioDetail = {
@@ -45,8 +48,38 @@ export class PortafolioDetailComponent implements OnInit {
   getPageData(){
     this.portafolioService.getDetail(this.slug).then(resp => {
       const {data} = resp
-      this.dataPage = data
-      console.log(this.dataPage)
+      this.dataPage = data;
+      this.getAnimations();
     })
+  }
+
+  getAnimations(){
+    gsap.from(".portafolio-anim", {
+      y: 100,
+      opacity: 0,
+      stagger: 0.3,
+      ease: 'expo',
+      duration: 3,
+      scrollTrigger: {
+        trigger: '.portafolio-anim',
+        start: '-200 center',
+      }
+    });
+    gsap.from(".item-text-mobile", {
+      x: 100,
+      opacity: 0,
+      stagger: 0.3,
+      ease: 'expo',
+      duration: 5,
+      scrollTrigger: {
+        trigger: '.item-text-mobile',
+        start: 'top center',
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    gsap.to(".anim-top", { clearProps: 'all' });
+    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
   }
 }
