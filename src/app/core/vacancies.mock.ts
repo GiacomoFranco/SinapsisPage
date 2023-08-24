cce0f81bc59fe3682bda51a3477fa13210aed2c7
Santy <santiago.gil@sinapsis.global>
2023-08-24 13:52:33 -0500

    Vacantes view, grid, inputs and responsive

---
+++ src/app/core/vacancies.mock.ts
@@ -0,0 +1,54 @@
+const VacanciesMock: any[] = [
+  {
+    rol: 'Lorem ipsum is simple texto of the 1',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 2',
+    equipo: 'Lorem ipsum is simple 1',
+    remoto: 'Lorem ipsum is simple',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 3',
+    equipo: 'Lorem ipsum is simple 1',
+    remoto: 'Lorem ipsum is simple A',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 4',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple A',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 5',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 6',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 7',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple A',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 8',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 9',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple',
+  },
+  {
+    rol: 'Lorem ipsum is simple texto of the 10',
+    equipo: 'Lorem ipsum is simple',
+    remoto: 'Lorem ipsum is simple',
+  },
+];
+
+export default VacanciesMock;
---
+++ src/app/modules/vacantes/vacantes.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VacantesComponent } from './page/vacantes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from "../../shared/shared.module";
+import { GridModule } from '@progress/kendo-angular-grid';
+import { AvaiableVacanciesComponent } from './avaiable-vacancies/avaiable-vacancies.component';
+import { TextBoxModule } from '@progress/kendo-angular-inputs';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+
+
+const routes: Routes = [
+  { path: 'vacantes', component: VacantesComponent }
+];
+
+
+
+@NgModule({
+  declarations: [VacantesComponent, AvaiableVacanciesComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule,
+    GridModule,
+    TextBoxModule,
+    ButtonsModule,
+    DropDownsModule,
+  ],
+})
+export class VacantesModule {}
