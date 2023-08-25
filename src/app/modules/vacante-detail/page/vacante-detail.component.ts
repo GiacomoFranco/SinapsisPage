fd8d6d2382935deccbf6b38346c59bb9114649ce
Santy <santiago.gil@sinapsis.global>
2023-08-25 16:03:33 -0500

    vacante section, responsive, and animations

---
+++ src/app/modules/vacante-detail/page/vacante-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vacante-detail',
+  templateUrl: './vacante-detail.component.html',
+  styleUrls: ['./vacante-detail.component.scss']
+})
+export class VacanteDetailComponent {
+
+}
--- src/app/shared/directives/scroll-reveal.directive.ts
+++ src/app/shared/directives/scroll-reveal.directive.ts
@@ -5,47 +5,46 @@ import { ScrollTrigger } from 'gsap/all';
 @Directive({
   selector: '[appScrollReveal]',
 })
-export class ScrollRevealDirective implements AfterViewInit
- {
+export class ScrollRevealDirective implements AfterViewInit {
   constructor(private element: ElementRef) {}
   @Input() x: number | string;
   @Input() y: number | string;
+  @Input() start: string = '-30% center';
+  @Input() duration: number | string = 1;
+  @Input() delay: number | string;
+  @Input() stagger: number | string | any = 0.2;
   @Input() selectorClassAnimation: string;
   @Input() markers: boolean = false;
 
-
-  @Input() set isPending(state: boolean){
-
+  @Input() set isPending(state: boolean) {
     if (!state) {
-
       this.animation();
     }
   }
 
   animation() {
     setTimeout(() => {
-        gsap.from(
-          this.selectorClassAnimation
-            ? `.${this.selectorClassAnimation}`
-            : this.element.nativeElement,
-          {
-            x: this.x,
-            y: this.y,
-            opacity: 0,
-            ease: 'power',
-            scrollTrigger: {
-              trigger: this.element.nativeElement,
-              start: '-30% center',
-              markers: this.markers,
-              // id: this.id,
-            },
-            duration: 1,
-            stagger: 0.2,
-          }
-        );
+      gsap.from(
+        this.selectorClassAnimation
+          ? `.${this.selectorClassAnimation}`
+          : this.element.nativeElement,
+        {
+          x: this.x,
+          y: this.y,
+          opacity: 0,
+          ease: 'power',
+          duration: this.duration,
+          stagger: this.stagger,
+          delay: this.delay,
+          scrollTrigger: {
+            trigger: this.element.nativeElement,
+            start: this.start,
+            markers: this.markers,
+          },
+        }
+      );
     }, 0);
   }
-
 
   ngAfterViewInit(): void {
     if (!this.isPending) {
