75f0d2dc0675d94dcc7fa06baa4ecf8a42ab95d5
mateo.castano <mateo.castano@sinapsis.global>
2023-12-12 10:51:49 -0500

    feat: Implement ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'sinapsis-web';
 
   hasRendered = false;
 
-    ngOnInit(): void {
-      this.hasRendered = true;
-    }
+  constructor(private translate: TranslateService) {
+    const userLang = navigator.language || 'en';
+    const languageCode = userLang.split('-')[0];
+    this.translate.setDefaultLang(languageCode);
+    this.translate.use(languageCode)
+  }
+
+  ngOnInit(): void {
+    this.hasRendered = true;
+  }
 }
